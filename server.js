df30ac0a67e3c96bc67ce153beaace95c4c92342
Anish kumar <kumaranish7119@gmail.com>
2026-04-20 15:01:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const decreasebtn = document.getElementById("decreasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const increasebtn = document.getElementById("increasebtn");
+let count = 0;
+increasebtn.onclick = function () {
+    count++;
+    countlabel.textContent = count;
+}
+decreasebtn.onclick = function () {
+    count--;
+    countlabel.textContent = count;
+}
+resetbtn.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
